d84a1bd0c0c99d803b4199f2f6716005eec44bf9
LeslieAine <aineleslie@gmail.com>
2023-02-07 13:29:28 +0300

    Add functionality js file

---
+++ src/modules/func.js
@@ -0,0 +1,32 @@
+const recentScoresList = document.querySelector('.recent-scores-list');
+const inputName = document.querySelector('input[name="name"]');
+const inputScore = document.querySelector('input[name="score"]');
+
+const refreshScores = () => {
+  const scores = [
+    { 
+        user: Leslie,
+        score: 334,
+    },
+    {
+        user: Aine,
+        score: 542,
+    }
+
+  ]
+
+  recentScoresList.innerHTML = '';
+
+  scores.result.forEach((score) => {
+    const li = document.createElement('li');
+    li.classList.add('recent-score');
+    li.innerHTML = `${score.user}: ${score.score}`;
+    recentScoresList.appendChild(li);
+  });
+
+  // reset input fields
+  inputName.value = '';
+  inputScore.value = '';
+};
+
+export default refreshScores;
