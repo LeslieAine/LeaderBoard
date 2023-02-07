5b38a3b29a4ee95e9e174239a2918b56e5960f92
LeslieAine <aineleslie@gmail.com>
2023-02-07 13:36:57 +0300

    Fix linters

--- src/modules/func.js
+++ src/modules/func.js
@@ -2,18 +2,21 @@ const recentScoresList = document.querySelector('.recent-scores-list');
 const inputName = document.querySelector('input[name="name"]');
 const inputScore = document.querySelector('input[name="score"]');
 
+const score1 = { user: 'Leslie', score: 332 };
+const score2 = { user: 'Leslie', score: 332 };
+
 const refreshScores = () => {
   const scores = [
-    { 
-        user: Leslie,
-        score: 334,
+    {
+      user: score1.user,
+      score: score1.score,
     },
     {
-        user: Aine,
-        score: 542,
-    }
+      user: score2.user,
+      score: score2.score,
+    },
 
-  ]
+  ];
 
   recentScoresList.innerHTML = '';
 
